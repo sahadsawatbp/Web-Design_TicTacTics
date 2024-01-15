425b20e4121c39e8e724844eb829e93dee1518a1
Sahadsawat Boonphuen <80098729+sahadsawatbp@users.noreply.github.com>
2024-01-16 00:55:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000
 app.use(express.static('public'))
 
 app.get('/',(req,res) =>{
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + 'index.html')
 })
 app.listen(port,() =>{
     console.log(`Example app listening on port ${port}`)
