7310466a5cdb08c28b3a804619df279308c3ff88
Sahadsawat Boonphuen <80098729+sahadsawatbp@users.noreply.github.com>
2024-02-20 05:01:05 +0700

    update

--- public/classes/option.js
+++ public/classes/option.js
@@ -1,12 +1,43 @@
+//npm install --save @google-cloud/storage
 const iconImg = document.getElementById("icon-img");
 const profileBox = document.getElementById("box-profile");
 const userName = document.getElementById("user-name");
 const playWithFriend = document.getElementById("play-with-friend");
 const playWithStranger = document.getElementById("play-with-stranger");
 const logoutBtn = document.getElementById("logout-btn")
+const popupScreen = document.getElementById("popup-container")
+const editProfile = document.getElementById("edit-profile")
+const popupSave = document.getElementById("popup-save")
+const popupCancel = document.getElementById("popup-cancel")
 const currentUser = firebase.auth().currentUser
 const userListRef = firebase.database().ref("UserList");
+const fileBtn = document.getElementById("fileInp")
+var fileName;
+var fileItem;
 
+function getFile(e){
+    fileItem = e.target.files[0];
+    fileName = fileItem.name;
+}
+
+let uploadImage = () => {
+    let storageRef = firebase.storage().ref("images/"+fileName);
+    let uploadTask = storageRef.put(fileItem);
+
+    uploadTask.on("state_changed",(snapshot)=>{
+        console.log(snapshot);
+    },(error)=>{
+        console.log("Eroor is ",error);
+    },() =>{
+        uploadTask.snapshot.ref.getDownloadURL().then((url)=>{
+            console.log("URL",url);
+        })
+    })
+}
+
+
+
+popupScreen.style.display = "none"
 profileBox.style.display = "none";
 userName.style.display = "flex";
 
@@ -37,6 +68,14 @@ playWithFriend.addEventListener("click",()=>{
 playWithStranger.addEventListener("click",()=>{
     window.location = "index.html"
 })
+editProfile.addEventListener("click",()=>{
+    popupScreen.style.display = "flex"
+})
+popupCancel.addEventListener("click",()=>{
+    popupScreen.style.display = "none"
+})
+popupSave.addEventListener("click",uploadImage)
+fileBtn.addEventListener("change",getFile)
 iconImg.addEventListener("click",toggleBox)
 if(logoutBtn){
     logoutBtn.addEventListener("click", signOut)
